0cec0ac5d83cd5642281dfb830f9f81970fb972e
RichardAndrade010101 <118805704+RichardAndrade010101@users.noreply.github.com>
2023-08-16 19:24:18 -0400

    desafio17.py

---
+++ desafio17.py
@@ -0,0 +1,16 @@
+#Solicitar a idade do usuário
+idade = int(input('Digite sua idade: '))
+
+#Verificar a faixa etária
+
+if idade < 13:
+    print('Você é uma criança.')
+
+elif idade <= 13 and <= 19:
+    print('Você é um adolescente.')
+
+else idade:
+
+    print('Você é um adulto.')
+
+
