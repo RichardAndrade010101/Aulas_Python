f9c020d3daf46739b43099d3a293c3ee33b480cf
RichardAndrade010101 <118805704+RichardAndrade010101@users.noreply.github.com>
2023-08-16 19:09:13 -0400

    desafio16.py

---
+++ desafio16.py
@@ -0,0 +1,8 @@
+# Solicitar um número do usuário
+numero = float(input("Digite um número: "))
+
+# Verificar se o número é maior que 10
+if numero > 10:
+    print("O número é maior que 10.")
+else:
+    print("O número é menor que 10.")
