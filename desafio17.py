000a0a0c1190abbe09fd9a5feaedd3744793495d
RichardAndrade010101 <andradericharde8@gmail.com>
2023-08-16 20:17:50 -0400

    dfjg

--- desafio17.py
+++ desafio17.py
@@ -6,8 +6,8 @@ idade = int(input('Digite sua idade: '))
 if idade < 13:
     print('Você é uma criança.')
 
-elif idade <= 13 and <= 19:
+elif 13 <= idade <= 19:
     print('Você é um adolescente.')
 
-else idade:
+else :
      print('Você é um adulto.')
