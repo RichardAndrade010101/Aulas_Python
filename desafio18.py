74704c1c42678a4156145f8dd85ff3a2ee86fdeb
RichardAndrade010101 <andradericharde8@gmail.com>
2023-08-16 20:26:36 -0400

    yfu

--- desafio18.py
+++ desafio18.py
@@ -1,11 +1,11 @@
 #lista de carros em estoque
-carros_em_estoque['BMW X6', 'BMW i5', 'BMW i8' ]
+carros_em_estoque = ['BMW X6', 'BMW i5', 'BMW i8' ]
 
 #Solicitar ao usuário o nome do carro desejado
 carro_desejado = input('Digite o nome do carro que deseja comprar: ')
 
 #Verificar se o carro está em estoque
-if carro_desejado in carro_em_estoque:
+if carro_desejado in carros_em_estoque:
     print('Este carro está disponível.')
 
 else:
