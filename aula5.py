e5bd7bee7f3ef3f51beefde453535d4676e96ed6
RichardAndrade010101 <andradericharde8@gmail.com>
2023-08-18 19:00:51 -0400

    djkfhkjedfhg

---
+++ aula5.py
@@ -0,0 +1,18 @@
+#operadores aritiméticos
+soma = 10+10
+print(soma)
+
+divisao = 10 / 2.2 #float
+print(divisao)
+
+div_inteira = 10 // 2.2
+print('divisão inteira: ', div_inteira)
+
+exponenciacao = 2 ** 10
+print('exponenciação: ', exponenciacao)
+
+modulo = 55 % 2 #resto da divisão
+print('Módulo: ', modulo)
+
+print(10 % 8 == 0) #informa se um número é divisível por outro
+print(16 % 8 == 0)
