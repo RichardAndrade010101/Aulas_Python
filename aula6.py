c9161edce684f045aeb84d050a07281a772b592b
RichardAndrade010101 <andradericharde8@gmail.com>
2023-08-21 19:42:14 -0400

    dfgh

---
+++ aula6.py
@@ -0,0 +1,20 @@
+'''
+#concatenação (+) e repetição (*) com operadores
+concatenacao = 'a' + 'b' + 'c' '\n'
+teste = 'zequinha' + ' ' + str(1) + ' ' + 'real.' #adicionei o str(1) para que fosse representado na frase
+print(concatenacao, teste)
+ 
+a_dez_vezes = 'a' * 10 #vai repetir
+tres_vezes = 3 * 'zezinho'
+print(a_dez_vezes)
+print(tres_vezes)
+'''
+#Precedência entre os operadores
+#1. (n + n)
+#2. **
+#3. * / // %
+#4. + -
+conta_1 = 1 + 1 ** 5 + 5
+print(conta_1)
+
+
---
+++ aula9.py
@@ -0,0 +1,18 @@
+#armazenar mais de uma informação em variáveis
+#manter a sequencia  dos dados em uma variável
+
+cidade1 = 'rio de janeiro'
+cidade2 = 'são paulo'
+cidade3 = 'salvador'
+                         #itens
+cidades = ['rio de janeiro', 'são paulo', 'salvador']
+#               0                1            2
+
+#cidades.append('santa catarina')
+#cidades.remove('salvador')
+#cidades.insert(2, 'manaus')
+#cidades.pop()
+cidades.clear()
+
+
+print(cidades)
